1fca51d68b23869ce59a5d6b947e0500afed7d78
kimgs1 <gooksong89@gmail.com>
2018-03-20 23:30:31 +0900

    00

--- src/main/webapp/js/penetrationsearch.js
+++ src/main/webapp/js/penetrationsearch.js
@@ -245,7 +245,7 @@ function drawPenetrationsearchGrid(result,dataList){
 			shrinkToFit : true,
 			scrollrows : true,
 			gridview : true,
-			height : 350,
+			height : 300,
 			viewrecords: true,
 			onSelectRow : function(id) {
 //				var rowdata = grid.jqGrid('getRowData', id);
